570ea72f64c57cbb9a91637ef32fb6880f272de3
mariovector <mariovector@gmail.com>
2021-12-13 15:35:31 -0300

    criando pasta com tarefa cálculo média

---
+++ MOD8/Tarefa_Calculo_Media/src/br/com/mario/media.java
@@ -0,0 +1,18 @@
+package br.com.mario;
+
+public class media {
+    public static void main(String[] args) {
+
+        int nota1 = 8;
+        int nota2 = 7;
+        double nota3 = 5.5;
+        double nota4 = 4.4;
+        double media = (nota1+nota2+nota3+nota4)/4;
+        System.out.println("sua média foi de: "+media);
+
+
+
+
+
+    }
+}
