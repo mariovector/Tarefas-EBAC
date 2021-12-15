1bc58d115be9bcffff1846df5a4554b4640d9cc6
mariovector <mariovector@gmail.com>
2021-12-15 10:50:09 -0300

    corrigindo nome da classe em camelcase

--- MOD8/Tarefa_Calculo_Media/src/br/com/mario/Media.java
+++ MOD8/Tarefa_Calculo_Media/src/br/com/mario/Media.java
@@ -1,6 +1,6 @@
 package br.com.mario;
 
-public class media {
+public class Media {
     public static void main(String[] args) {
 
         int nota1 = 8;
@@ -8,7 +8,7 @@ public class media {
         double nota3 = 5.5;
         double nota4 = 4.4;
         double media = (nota1+nota2+nota3+nota4)/4;
-        System.out.println("sua mÃ©dia foi de: "+media);
+        System.out.println("sua média foi de: "+media);
 
 
 
